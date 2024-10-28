43c8281323392582584f9dd734b2f1c682ecb9f2
Romalvo <roman.voronkov.06@gmail.com>
2024-10-28 11:13:44 +0200

    Updated TransactionController

--- src/main/java/sda/spring/basicbankingsystem/controller/TransactionController.java
+++ src/main/java/sda/spring/basicbankingsystem/controller/TransactionController.java
@@ -1,18 +1,26 @@
 package sda.spring.basicbankingsystem.controller;
 
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sda.spring.basicbankingsystem.dto.request.IntraBankTransferRequestDto;
+import sda.spring.basicbankingsystem.service.TransactionService;
 
 @RestController
 @RequestMapping("/api/transactions")
 public class TransactionController {
 
-    public TransactionController() {
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+
+        this.transactionService = transactionService;
 
     }
-
-    public void intraBankPayment() {
+    @PostMapping("/intra-bank")
+    public void intraBankPayment(@RequestBody IntraBankTransferRequestDto intraBankTransferRequestDto) {
 
     }
 
