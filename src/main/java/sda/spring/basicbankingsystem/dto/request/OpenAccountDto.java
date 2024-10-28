d6d684694af66492813e7e5158aabadec85995e0
Romalvo <roman.voronkov.06@gmail.com>
2024-10-28 11:13:08 +0200

    Updated OpenAccountDto

--- src/main/java/sda/spring/basicbankingsystem/dto/request/OpenAccountDto.java
+++ src/main/java/sda/spring/basicbankingsystem/dto/request/OpenAccountDto.java
@@ -1,9 +1,15 @@
 package sda.spring.basicbankingsystem.dto.request;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
+@AllArgsConstructor
+@NoArgsConstructor
 public class OpenAccountDto {
 
     private Long branchId;
